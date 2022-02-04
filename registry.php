f9cefd367bc761df1a7595273ae99331ed0650a4
aleksa2450 <aleksa2450@yandex.ru>
2022-02-04 20:46:22 +0300

    Добавление Регистри

---
+++ registry.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+error_reporting(-1);
+
+require __DIR__ . '/config/config.php';
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Hello, world!</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">;
+    <link rel="stylesheet" href="assets/css/style.css">;
+</head>
+<body>
+
+<div class="container">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/">DEMO-SITE</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Главная <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Вход</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="registry.php">Регистрация</a>
+                    </li>
+
+
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+
+    <div class="wrapper mt-5">
+
+    </div>
+
+    <hr>
+    <footer>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="text-center">
+                    <p>© <?php echo date('Y')?> DEMO-SITE</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+
+
+<script src="assets/js/jquery-1.12.4.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>>
+</body>
+</html>
+
