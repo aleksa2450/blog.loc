aff4887a734c910ebddf940aea061577f0fd1a19
aleksa2450 <aleksa2450@yandex.ru>
2022-02-04 20:11:36 +0300

    Добавлен шаблон html

--- index.php
+++ index.php
@@ -4,3 +4,27 @@ error_reporting(-1);
 
 require __DIR__ . '/config/config.php';
 
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Hello, world!</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">;
+    <link rel="stylesheet" href="assets/css/style.css">;
+</head>
+<body>
+<h1>Hello, world!</h1>
+
+
+<script src="assets/js/jquery-1.12.4.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>>
+</body>
+</html>
